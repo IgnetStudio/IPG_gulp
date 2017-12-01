056443b2c92d0c57e176556cefc0add3494ebc3f
IgnetStudio <j.ignatowicz@wit.edu.pl>
2017-12-01 23:56:25 +0100

    tasks init

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,17 @@
 // GULP PLUGINS
 //
 
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const replace = require('gulp-replace');
+const cleanCss = require('gulp-clean-css');
+const stripCssComments = require('gulp-strip-css-comments');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const posthtml = require('gulp-posthtml');
+const deleteLines = require('gulp-delete-lines');
+
 //
 // GULP SETUP
 //
